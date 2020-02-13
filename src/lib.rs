0801bace50af34ffea8acec180fabb094df49eb5
Chris Seymour <christopher.seymour@nanoporetech.com>
2020-02-13 11:24:14 +0000

    untabify

--- src/lib.rs
+++ src/lib.rs
@@ -35,74 +35,74 @@ fn beam_search_<D: Data<Elem=f32>>(result: &ArrayBase<D, Ix2>, alphabet: String,
     let mut new_probs = Vec::new();
 
     for pr in result.slice(s![..;-1, ..]).outer_iter() {
-	new_probs.clear();
+        new_probs.clear();
 
-	for &(beam, base_prob, n_prob) in &cur_probs {
-	    // add N to beam
-	    if pr[0] > beam_cut_threshold {
-		new_probs.push((beam, 0.0, (n_prob + base_prob) * pr[0]));
-	    }
+        for &(beam, base_prob, n_prob) in &cur_probs {
+            // add N to beam
+            if pr[0] > beam_cut_threshold {
+                new_probs.push((beam, 0.0, (n_prob + base_prob) * pr[0]));
+            }
 
-	    for b in 1..alphabet.len() {
-		if pr[b] < beam_cut_threshold {
-		    continue
-		}
-		if b == beam_prevs[beam as usize].0 {
-		    new_probs.push((beam, base_prob * pr[b], 0.0));
-		    let mut new_beam = beam_forward[beam as usize][b-1];
-		    if new_beam == -1 {
-			new_beam = beam_prevs.len() as i32;
-			beam_prevs.push((b, beam));
-			beam_forward[beam as usize][b-1] = new_beam;
-			beam_forward.push(vec![-1; alphabet_size]);
-		    }
+            for b in 1..alphabet.len() {
+                if pr[b] < beam_cut_threshold {
+                    continue
+                }
+                if b == beam_prevs[beam as usize].0 {
+                    new_probs.push((beam, base_prob * pr[b], 0.0));
+                    let mut new_beam = beam_forward[beam as usize][b-1];
+                    if new_beam == -1 {
+                        new_beam = beam_prevs.len() as i32;
+                        beam_prevs.push((b, beam));
+                        beam_forward[beam as usize][b-1] = new_beam;
+                        beam_forward.push(vec![-1; alphabet_size]);
+                    }
 
-		    new_probs.push((new_beam, n_prob * pr[b], 0.0));
+                    new_probs.push((new_beam, n_prob * pr[b], 0.0));
 
-		} else {
-		    let mut new_beam = beam_forward[beam as usize][b-1];
-		    if new_beam == -1 {
-			new_beam = beam_prevs.len() as i32;
-			beam_prevs.push((b, beam));
-			beam_forward[beam as usize][b-1] = new_beam;
-			beam_forward.push(vec![-1; alphabet_size]);
-		    }
+                } else {
+                    let mut new_beam = beam_forward[beam as usize][b-1];
+                    if new_beam == -1 {
+                        new_beam = beam_prevs.len() as i32;
+                        beam_prevs.push((b, beam));
+                        beam_forward[beam as usize][b-1] = new_beam;
+                        beam_forward.push(vec![-1; alphabet_size]);
+                    }
 
-		    new_probs.push((new_beam, (base_prob + n_prob) * pr[b], 0.0));
-		}
-	    }
-	}
-	std::mem::swap(&mut cur_probs, &mut new_probs);
+                    new_probs.push((new_beam, (base_prob + n_prob) * pr[b], 0.0));
+                }
+            }
+        }
+        std::mem::swap(&mut cur_probs, &mut new_probs);
 
-	cur_probs.sort_by_key(|x| x.0);
-	let mut last_key: i32 = -1;
-	let mut last_key_pos = 0;
-	for i in 0..cur_probs.len() {
-	    if cur_probs[i].0 == last_key {
-		cur_probs[last_key_pos].1 = cur_probs[last_key_pos].1 + cur_probs[i].1;
-		cur_probs[last_key_pos].2 = cur_probs[last_key_pos].2 + cur_probs[i].2;
-		cur_probs[i].0 = -1;
-	    } else {
-		last_key_pos = i;
-		last_key = cur_probs[i].0;
-	    }
-	}
+        cur_probs.sort_by_key(|x| x.0);
+        let mut last_key: i32 = -1;
+        let mut last_key_pos = 0;
+        for i in 0..cur_probs.len() {
+            if cur_probs[i].0 == last_key {
+                cur_probs[last_key_pos].1 = cur_probs[last_key_pos].1 + cur_probs[i].1;
+                cur_probs[last_key_pos].2 = cur_probs[last_key_pos].2 + cur_probs[i].2;
+                cur_probs[i].0 = -1;
+            } else {
+                last_key_pos = i;
+                last_key = cur_probs[i].0;
+            }
+        }
 
-	cur_probs.retain(|x| x.0 != -1);
-	cur_probs.sort_by(|a, b| (b.1 + b.2).partial_cmp(&(a.1 + a.2)).unwrap());
-	cur_probs.truncate(beam_size);
-	let top = cur_probs[0].1 + cur_probs[0].2;
-	for mut x in &mut cur_probs {
-	    x.1 /= top;
-	    x.2 /= top;
-	}
+        cur_probs.retain(|x| x.0 != -1);
+        cur_probs.sort_by(|a, b| (b.1 + b.2).partial_cmp(&(a.1 + a.2)).unwrap());
+        cur_probs.truncate(beam_size);
+        let top = cur_probs[0].1 + cur_probs[0].2;
+        for mut x in &mut cur_probs {
+            x.1 /= top;
+            x.2 /= top;
+        }
     }
 
     let mut out = String::new();
     let mut beam = cur_probs[0].0;
     while beam != 0 {
-	out.push(alphabet[beam_prevs[beam as usize].0]);
-	beam = beam_prevs[beam as usize].1;
+        out.push(alphabet[beam_prevs[beam as usize].0]);
+        beam = beam_prevs[beam as usize].1;
     }
     out
 }
