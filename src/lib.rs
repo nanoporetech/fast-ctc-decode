e6cd4bbdecb11496ef135cf6f14edcec24b704ca
Alex Merry <alex.merry@nanoporetech.com>
2020-04-01 09:50:53 +0100

    Reduce duplication in the documentation
    Move most of the general background info, as well as descriptions of
    common arguments, to the module-level docstrings.

--- src/lib.rs
+++ src/lib.rs
@@ -40,26 +40,8 @@ impl std::error::Error for SearchError {}
 
 /// Perform a CTC beam search decode on an RNN output.
 ///
-/// The ultimate aim here is to find a way of labelling some input data. In the case of nanopore
-/// sequencing data, we're taking the electrical current samples, and labelling them with what we
-/// think the DNA/RNA base is at any given time.
-///
-/// CTC decoding (and hence this function) takes as input the result of a neural network that has
-/// figured out, for each sample and each label, the probability that the sample corresponds to
-/// that label. It also outputs a probability for the data point corresponding to an extra "blank"
-/// label (a sort of "none of the above" option). This is represented as a 2D matrix of size ``N x
-/// (L+1)``, where ``N`` is the number of samples and ``L`` is the number of labels we're
-/// interested in (the ``+1`` is to account for the blank label).
-///
-/// What this function does is a beam search variant of the prefix search decoding mentioned (and
-/// described in fairly vague terms) in the original CTC paper (Graves et al, 2006, section 3.2).
-///
-/// A quick overview: a _path_ through the matrix is an assignment of a label or blank to each
-/// sample. The probability that the path is correct is the product of the selected entries in the
-/// matrix. Each path produces a labelling: first collapse all duplicate labels or blanks, then
-/// remove the remaining blanks - AAAGGbGGbbbC would become AGbGbC, and then AGGC. The probability
-/// that the labelling is correct is the sum of the probabilities of the paths that produce it. We
-/// want the most likely labelling.
+/// This function does a beam search variant of the prefix search decoding mentioned (and described
+/// in fairly vague terms) in the original CTC paper (Graves et al, 2006, section 3.2).
 ///
 /// The paper mentioned above provides recursive equations that give an efficient way to find the
 /// probability for a specific labelling. A tree of possible labelling suffixes, together with
@@ -68,14 +50,12 @@ impl std::error::Error for SearchError {}
 /// step, we ignore all but the most-probable tree leaves (like searching with a torch beam). This
 /// means we may not actually find the most likely labelling, but it often works very well.
 ///
+/// See the module-level documentation for general requirements on `network_output` and `alphabet`.
+///
 /// Args:
-///     network_output (numpy.ndarray): The 2D array output of the neural network. Must be the
-///         output of a softmax layer, with values between 0.0 and 1.0 representing probabilities.
-///         The first (outer) axis is time, and the second (inner) axis is label. The first entry
-///         on the label axis is the blank label.
-///     alphabet (sequence): The labels (including the blank label) in the order given on the label
-///          axis of `network_output`. Length must match the size of the inner axis of
-///          `network_output`.
+///     network_output (numpy.ndarray): The 2D array output of the neural network.
+///     alphabet (sequence): The labels (including the blank label, which must be first) in the
+///         order given on the inner axis of `network_output`.
 ///     beam_size (int): How many search points should be kept at each step. Higher numbers are
 ///         less likely to discard the true labelling, but also make it slower and more memory
 ///         intensive. Must be at least 1.
@@ -133,16 +113,12 @@ fn beam_search(
 /// or two different networks run over the same input, for example.
 ///
 /// Args:
-///     network_output_1 (numpy.ndarray): The 2D array output of the neural network. Must be the
-///         output of a softmax layer, with values between 0.0 and 1.0 representing probabilities.
-///         The first (outer) axis is time, and the second (inner) axis is label. The first entry
-///         on the label axis is the blank label.
-///     network_output_2 (numpy.ndarray): The second neural network output. The same constraints
-///         apply as to `network_output_1`. Additionally, the inner axis size must be the same as
-///         that of `network_output_1` (but the outer axis can be a different size).
-///     alphabet (str): The labels (including the blank label) in the order given on the label axis
-///         of `network_output`. Length must match the size of the inner axis of
-///         `network_output_1` and `network_output_2`.
+///     network_output_1 (numpy.ndarray): The 2D array output of the first neural network.
+///     network_output_2 (numpy.ndarray): The 2D array output of the second neural network. Note
+///         that while the inner axis size must match that of `network_output_1`, the outer axis
+///         can be a different size.
+///     alphabet (str): The labels (including the blank label, which must be first) in the order
+///         given on the inner axis of `network_output_1` and `network_output_2`.
 ///     envelope (numpy.ndarray, optional): An Nx2 array, where N is the outer axis length of
 ///         `network_output_1`. For each row of `network_output_1`, this gives the starting and
 ///         ending rows of `network_output_2` to consider for alignment.
@@ -228,6 +204,42 @@ fn beam_search_2d(
 }
 
 /// Methods for labelling RNN results using CTC decoding.
+///
+/// The methods in this module implement the last step of labelling input data. In the case of
+/// nanopore sequencing data, we're taking the electrical current samples, and labelling them with
+/// what we think the DNA/RNA base is at any given time.
+///
+/// CTC decoding (and hence the funtions in this module) takes as input the result of a neural
+/// network that has figured out, for each sample and each label, the probability that the sample
+/// corresponds to that label. The network also outputs a probability for the data point
+/// corresponding to an extra "blank" label (a sort of "none of the above" option). This is
+/// represented as a 2D matrix of size ``N x (L+1)``, where ``N`` is the number of samples and
+/// ``L`` is the number of labels we're interested in (the ``+1`` is to account for the blank
+/// label).
+///
+/// A _path_ through the matrix is an assignment of a label or blank to each sample. The
+/// probability that the path is correct is the product of the selected entries in the matrix. Each
+/// path produces a labelling: first collapse all duplicate labels or blanks, then remove the
+/// remaining blanks - AAAGGbGGbbbC would become AGbGbC, and then AGGC. The probability that the
+/// labelling is correct is the sum of the probabilities of the paths that produce it. We want the
+/// most likely labelling.
+///
+/// This problem is, in general, intractable. This module provides heuristic functions that attempt
+/// to find the most likely labelling (but may produce a suboptimal labelling).
+///
+/// All functions take outputs from one or more neural networks, plus an alphabet to use for the
+/// labelling.
+///
+/// The network outpus are 2D arrays produced by a softmax layer of a neural network, with values
+/// between 0.0 and 1.0 representing probabilities. The outer axis (rows) is time, and the inner
+/// axis (columns) is labels. The first entry on the label axis is assumed to be the blank label.
+/// It's also worth noting that the values in each row should sum to 1.0.
+///
+/// The alphabet can be a str or any sequence of str (eg: a list or tuple of str). Each element (or
+/// character in the case of str) provides the labelling for one element of the inner axis of the
+/// network output(s) - therefore, len(alphabet) must be the size of that inner axis. Using a list
+/// or tuple allows multi-character labels to be specified. Note that the first label is not
+/// actually used by any of the functions in this module, so the value does not matter.
 #[pymodule]
 fn fast_ctc_decode(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(beam_search))?;
