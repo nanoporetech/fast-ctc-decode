37b1fb7b4997fe3092a4476db11855c346461e53
David Muhr <muhrdavid@gmail.com>
2022-10-10 18:46:42 +0200

    build wheels for more platforms

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e -x
 
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+rustup default 1.54.0
+
 for PYBIN in /opt/python/cp3[67891]*/bin; do
     "${PYBIN}/pip" install maturin
     "${PYBIN}/maturin" build -i "${PYBIN}/python" --release
