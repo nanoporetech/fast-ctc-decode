9b83cb40ea815fe556ff51990108b48d6674068e
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-04-01 19:31:27 +0100

    cargo build for debug

--- .github/workflows/build-wheels.sh
+++ .github/workflows/build-wheels.sh
@@ -5,6 +5,8 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup default 1.68.2
 
+cargo build
+
 for PYBIN in /opt/python/cp3[7891]*/bin; do
     "${PYBIN}/pip" install maturin
     "${PYBIN}/maturin" build -F python -i "${PYBIN}/python" --release
